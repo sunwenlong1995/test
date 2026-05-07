89501f71a4cc1d79d8a89885c21bb4e4b447db2b
sunwenlong12138 <sunwenlong12138@example.com>
2026-05-07 14:55:32 +0800

    Update project structure for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3009',
-        changeOrigin: true
-      }
-    }
+  build: {
+    outDir: 'dist'
   }
 })
